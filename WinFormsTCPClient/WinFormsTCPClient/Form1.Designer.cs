4f8c264a23f42d5d0a506780836594c470c8cbe5
WiljamiHakkarainen <hakwiljami@gmail.com>
2025-02-19 16:19:30 +0200

    First commit

---
+++ WinFormsTCPClient/WinFormsTCPClient/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WinFormsTCPClient
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.LogListBox = new System.Windows.Forms.ListBox();
+            this.robotPanel = new System.Windows.Forms.Panel();
+            this.robotLabel = new System.Windows.Forms.Label();
+            this.lstatPanel = new System.Windows.Forms.Panel();
+            this.lstatLabel = new System.Windows.Forms.Label();
+            this.cranePanel = new System.Windows.Forms.Panel();
+            this.craneLabel = new System.Windows.Forms.Label();
+            this.mcentPanel = new System.Windows.Forms.Panel();
+            this.mcentLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.robotPercentageTextBox = new System.Windows.Forms.TextBox();
+            this.lstatPercentageTextBox = new System.Windows.Forms.TextBox();
+            this.cranePercentageTextBox = new System.Windows.Forms.TextBox();
+            this.mcentPercentageTextBox = new System.Windows.Forms.TextBox();
+            this.robotPanel.SuspendLayout();
+            this.lstatPanel.SuspendLayout();
+            this.cranePanel.SuspendLayout();
+            this.mcentPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(27, 30);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(131, 30);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // LogListBox
+            // 
+            this.LogListBox.FormattingEnabled = true;
+            this.LogListBox.Location = new System.Drawing.Point(27, 59);
+            this.LogListBox.Name = "LogListBox";
+            this.LogListBox.Size = new System.Drawing.Size(392, 368);
+            this.LogListBox.TabIndex = 2;
+            // 
+            // robotPanel
+            // 
+            this.robotPanel.BackColor = System.Drawing.Color.Gray;
+            this.robotPanel.Controls.Add(this.robotLabel);
+            this.robotPanel.Location = new System.Drawing.Point(445, 59);
+            this.robotPanel.Name = "robotPanel";
+            this.robotPanel.Size = new System.Drawing.Size(100, 100);
+            this.robotPanel.TabIndex = 3;
+            // 
+            // robotLabel
+            // 
+            this.robotLabel.AutoSize = true;
+            this.robotLabel.Location = new System.Drawing.Point(35, 40);
+            this.robotLabel.Name = "robotLabel";
+            this.robotLabel.Size = new System.Drawing.Size(36, 13);
+            this.robotLabel.TabIndex = 0;
+            this.robotLabel.Text = "Robot";
+            // 
+            // lstatPanel
+            // 
+            this.lstatPanel.BackColor = System.Drawing.Color.Gray;
+            this.lstatPanel.Controls.Add(this.lstatLabel);
+            this.lstatPanel.Location = new System.Drawing.Point(564, 59);
+            this.lstatPanel.Name = "lstatPanel";
+            this.lstatPanel.Size = new System.Drawing.Size(100, 100);
+            this.lstatPanel.TabIndex = 4;
+            // 
+            // lstatLabel
+            // 
+            this.lstatLabel.AutoSize = true;
+            this.lstatLabel.Location = new System.Drawing.Point(35, 40);
+            this.lstatLabel.Name = "lstatLabel";
+            this.lstatLabel.Size = new System.Drawing.Size(30, 13);
+            this.lstatLabel.TabIndex = 0;
+            this.lstatLabel.Text = "Lstat";
+            // 
+            // cranePanel
+            // 
+            this.cranePanel.BackColor = System.Drawing.Color.Gray;
+            this.cranePanel.Controls.Add(this.craneLabel);
+            this.cranePanel.Location = new System.Drawing.Point(674, 59);
+            this.cranePanel.Name = "cranePanel";
+            this.cranePanel.Size = new System.Drawing.Size(100, 100);
+            this.cranePanel.TabIndex = 5;
+            // 
+            // craneLabel
+            // 
+            this.craneLabel.AutoSize = true;
+            this.craneLabel.Location = new System.Drawing.Point(35, 40);
+            this.craneLabel.Name = "craneLabel";
+            this.craneLabel.Size = new System.Drawing.Size(35, 13);
+            this.craneLabel.TabIndex = 0;
+            this.craneLabel.Text = "Crane";
+            // 
+            // mcentPanel
+            // 
+            this.mcentPanel.BackColor = System.Drawing.Color.Gray;
+            this.mcentPanel.Controls.Add(this.mcentLabel);
+            this.mcentPanel.Location = new System.Drawing.Point(788, 59);
+            this.mcentPanel.Name = "mcentPanel";
+            this.mcentPanel.Size = new System.Drawing.Size(100, 100);
+            this.mcentPanel.TabIndex = 6;
+            // 
+            // mcentLabel
+            // 
+            this.mcentLabel.AutoSize = true;
+            this.mcentLabel.Location = new System.Drawing.Point(35, 40);
+            this.mcentLabel.Name = "mcentLabel";
+            this.mcentLabel.Size = new System.Drawing.Size(37, 13);
+            this.mcentLabel.TabIndex = 0;
+            this.mcentLabel.Text = "Mcent";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox1.Location = new System.Drawing.Point(585, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 32);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Active machine";
+            // 
+            // robotPercentageTextBox
+            // 
+            this.robotPercentageTextBox.Location = new System.Drawing.Point(445, 170);
+            this.robotPercentageTextBox.Name = "robotPercentageTextBox";
+            this.robotPercentageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.robotPercentageTextBox.TabIndex = 11;
+            // 
+            // lstatPercentageTextBox
+            // 
+            this.lstatPercentageTextBox.Location = new System.Drawing.Point(567, 170);
+            this.lstatPercentageTextBox.Name = "lstatPercentageTextBox";
+            this.lstatPercentageTextBox.Size = new System.Drawing.Size(97, 20);
+            this.lstatPercentageTextBox.TabIndex = 12;
+            // 
+            // cranePercentageTextBox
+            // 
+            this.cranePercentageTextBox.Location = new System.Drawing.Point(673, 170);
+            this.cranePercentageTextBox.Name = "cranePercentageTextBox";
+            this.cranePercentageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cranePercentageTextBox.TabIndex = 13;
+            // 
+            // mcentPercentageTextBox
+            // 
+            this.mcentPercentageTextBox.Location = new System.Drawing.Point(786, 170);
+            this.mcentPercentageTextBox.Name = "mcentPercentageTextBox";
+            this.mcentPercentageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mcentPercentageTextBox.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.mcentPercentageTextBox);
+            this.Controls.Add(this.cranePercentageTextBox);
+            this.Controls.Add(this.lstatPercentageTextBox);
+            this.Controls.Add(this.robotPercentageTextBox);
+            this.Controls.Add(this.mcentPanel);
+            this.Controls.Add(this.cranePanel);
+            this.Controls.Add(this.lstatPanel);
+            this.Controls.Add(this.robotPanel);
+            this.Controls.Add(this.LogListBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.robotPanel.ResumeLayout(false);
+            this.robotPanel.PerformLayout();
+            this.lstatPanel.ResumeLayout(false);
+            this.lstatPanel.PerformLayout();
+            this.cranePanel.ResumeLayout(false);
+            this.cranePanel.PerformLayout();
+            this.mcentPanel.ResumeLayout(false);
+            this.mcentPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.ListBox LogListBox;
+        private System.Windows.Forms.Panel robotPanel;
+        private System.Windows.Forms.Panel lstatPanel;
+        private System.Windows.Forms.Panel cranePanel;
+        private System.Windows.Forms.Panel mcentPanel;
+        private System.Windows.Forms.Label robotLabel;
+        private System.Windows.Forms.Label lstatLabel;
+        private System.Windows.Forms.Label craneLabel;
+        private System.Windows.Forms.Label mcentLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox robotPercentageTextBox;
+        private System.Windows.Forms.TextBox lstatPercentageTextBox;
+        private System.Windows.Forms.TextBox cranePercentageTextBox;
+        private System.Windows.Forms.TextBox mcentPercentageTextBox;
+    }
+}
---
+++ WinFormsTCPClient/WinFormsTCPClient/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsTCPClient
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private CancellationTokenSource cancellationTokenSource;
+        private List<Dictionary<string, object>> messages;
+        private int currentCommandIndex = 0;
+        private Dictionary<string, int> machineActivity;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMessages();
+            InitializeMachineActivity();
+        }
+        // Initialize the list of messages to be sent to the server
+        private void InitializeMessages()
+        {
+            messages = new List<Dictionary<string, object>>
+            {
+                new Dictionary<string, object> { { "device", "robot" }, { "routine", "pickFromEP" } },
+                new Dictionary<string, object> { { "device", "robot" }, { "routine", "placeToMP" } },
+                new Dictionary<string, object> { { "device", "lstat" }, { "routine", "rotateTable" } },
+                new Dictionary<string, object> { { "device", "lstat" }, { "routine", "moveIn" } },
+                new Dictionary<string, object> { { "device", "crane" }, { "routine", "pickFromLS" } },
+                new Dictionary<string, object> { { "device", "mcent" }, { "routine", "openDoors" } },
+                new Dictionary<string, object> { { "device", "crane" }, { "routine", "placeToMC" } },
+                new Dictionary<string, object> { { "device", "mcent" }, { "routine", "closeDoors" } },
+                new Dictionary<string, object> { { "device", "mcent" }, { "routine", "rotateTable" } },
+                new Dictionary<string, object> { { "device", "mcent" }, { "routine", "rotateTable" } },
+                new Dictionary<string, object> { { "device", "mcent" }, { "routine", "openDoors" } },
+                new Dictionary<string, object> { { "device", "crane" }, { "routine", "pickFromMC" } },
+                new Dictionary<string, object> { { "device", "mcent" }, { "routine", "closeDoors" } },
+                new Dictionary<string, object> { { "device", "crane" }, { "routine", "placeToLS" } },
+                new Dictionary<string, object> { { "device", "lstat" }, { "routine", "rotateTable" } },
+                new Dictionary<string, object> { { "device", "lstat" }, { "routine", "moveOut" } },
+                new Dictionary<string, object> { { "device", "robot" }, { "routine", "pickFromMP" } },
+                new Dictionary<string, object> { { "device", "robot" }, { "routine", "placeToWP" } },
+                new Dictionary<string, object> { { "device", "robot" }, { "routine", "pickFromWP" } },
+                new Dictionary<string, object> { { "device", "robot" }, { "routine", "placeToEP" } },
+            };
+        }
+        // Initialize the machine activity dictionary to track the number of tasks each
+        private void InitializeMachineActivity()
+        {
+            machineActivity = new Dictionary<string, int>
+            {
+                { "robot", 0 },
+                { "lstat", 0 },
+                { "crane", 0 },
+                { "mcent", 0 }
+            };
+        }
+        // Highlight the panel corresponding to the active machine
+        private void HighlightMachine(string device)
+        {
+            robotPanel.BackColor = device == "robot" ? Color.Green : Color.Gray;
+            lstatPanel.BackColor = device == "lstat" ? Color.Green : Color.Gray;
+            cranePanel.BackColor = device == "crane" ? Color.Green : Color.Gray;
+            mcentPanel.BackColor = device == "mcent" ? Color.Green : Color.Gray;
+        }
+        // Event handler for the Start button click event
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource?.Dispose();
+            cancellationTokenSource = new CancellationTokenSource();
+            StartButton.Enabled = false;
+            StopButton.Enabled = true;
+            Log("Attempting to connect to server...");
+            currentCommandIndex = 0;
+            await ProcessMessages(cancellationTokenSource.Token);
+        }
+        // Event handler for the Stop button click event
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource?.Cancel();
+            Log("Stopping TCP communication...");
+            StopConnection();
+            StartButton.Enabled = true;
+            StopButton.Enabled = false;
+        }
+        // Process the list of messages and send them to the server (loops through all tasks 10 times) 
+        private async Task ProcessMessages(CancellationToken token)
+        {
+            try
+            {
+                for (int loop = 0; loop < 10; loop++) // Outer loop to repeat tasks 10 times
+                {
+                    currentCommandIndex = 0; // Reset command index for each loop
+                    while (currentCommandIndex < messages.Count)
+                    {
+                        if (token.IsCancellationRequested) break;
+
+                        // Ensure we create a fresh connection for each message
+                        client?.Close();
+                        client = new TcpClient("127.0.0.1", 30000);
+                        stream = client.GetStream();
+                        Log("Connected to server.");
+
+                        var message = messages[currentCommandIndex];
+
+                        // Highlight the machine in use
+                        HighlightMachine(message["device"].ToString());
+
+                        // Increment the machine activity count
+                        machineActivity[message["device"].ToString()]++;
+
+                        // Serialize message
+                        string request = JsonSerializer.Serialize(message) + "\n";
+
+                        // Start stopwatch
+                        var stopwatch = Stopwatch.StartNew();
+
+                        // Send message
+                        byte[] data = Encoding.UTF8.GetBytes(request);
+                        await stream.WriteAsync(data, 0, data.Length, token);
+                        await stream.FlushAsync(token);
+
+                        // Remove outer braces and quotes from the JSON string and add spaces after keys
+                        string formattedRequest = request.Trim().TrimStart('{').TrimEnd('}').Replace("\"device\":", "device: ").Replace("\"routine\":", " routine: ").Replace("\"", "");
+                        Log($"SEND: {formattedRequest}");
+
+                        var buffer = new byte[4096];
+                        int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, token);
+
+                        // Stop stopwatch
+                        stopwatch.Stop();
+
+                        // Calculate and log the percentages
+                        int totalTasks = machineActivity["robot"] + machineActivity["lstat"] + machineActivity["crane"] + machineActivity["mcent"];
+                        foreach (var machine in machineActivity.Keys)
+                        {
+                            double percentage = (double)machineActivity[machine] / totalTasks * 100;
+                            UpdateMachinePercentageLabel(machine, percentage);
+                        }
+
+                        if (bytesRead > 0)
+                        {
+                            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                            // Remove outer braces and quotes from the JSON string and remove the "result" part
+                            string formattedResponse = response.Trim().TrimStart('{').TrimEnd('}').Replace("\"result\":", "").Replace("\"", "").Trim().TrimStart(',').Trim();
+                            Log($"FULL SERVER RESPONSE: {formattedResponse}");
+                            Log($"Task Duration: {stopwatch.ElapsedMilliseconds} ms");
+                            currentCommandIndex++;
+                        }
+                        else
+                        {
+                            Log("No response received from server.");
+                            break;
+                        }
+
+                        // Fully close stream after each message
+                        StopConnection();
+                        await Task.Delay(500, token);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Log($"Connection error: {ex.Message}");
+            }
+            finally
+            {
+                Log("All tasks completed. Closing connection...");
+                StopConnection();
+            }
+        }
+
+        // Updates the machines' activity percentage labels after each task
+        private void UpdateMachinePercentageLabel(string machine, double percentage)
+        {
+            string percentageText = $"{percentage:F2}%";
+            switch (machine)
+            {
+                case "robot":
+                    robotPercentageTextBox.Text = percentageText;
+                    break;
+                case "lstat":
+                    lstatPercentageTextBox.Text = percentageText;
+                    break;
+                case "crane":
+                    cranePercentageTextBox.Text = percentageText;
+                    break;
+                case "mcent":
+                    mcentPercentageTextBox.Text = percentageText;
+                    break;
+            }
+        }
+        // Stops connection with server when stop button is clicked
+        private void StopConnection()
+        {
+            stream?.Close();
+            client?.Close();
+            stream = null;
+            client = null;
+        }
+        // Log messages to the ListBox control
+        private void Log(string message)
+        {
+            if (LogListBox.InvokeRequired)
+            {
+                LogListBox.Invoke((Action)(() => Log(message)));
+            }
+            else
+            {
+                LogListBox.Items.Add(message);
+                LogListBox.TopIndex = LogListBox.Items.Count - 1;
+            }
+        }
+    }
+}
+
+
+
